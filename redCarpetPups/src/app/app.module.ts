92e7a201913751a7f8a8e15b0b26d2515c08edb9
Robert Black <robert2black@gmail.com>
2022-05-02 19:33:14 -0700

    convert to angular
    attempt to convert to angular project

---
+++ redCarpetPups/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BookingComponent,
+    CardsComponent,
+    CarouselComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
